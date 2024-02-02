0bf048938fb74c5a4bdd6acc9cd6b8e12be110d6
crolbar <crolbar@crolbar.com>
2024-02-02 11:54:05 +0200

    Update src/update.rs

--- src/update.rs
+++ src/update.rs
@@ -1,28 +1,32 @@
-use crossterm::event::{KeyCode, Event, self, KeyModifiers};
+use crossterm::event::{KeyCode, Event, self, KeyModifiers, KeyEventKind};
 use crate::{app::App, tui::Tui};
 use std::io::Result;
 
 pub fn update(app: &mut App, _tui: &mut Tui) -> Result<()> {
     if let Event::Key(key) = event::read()? {
+        if key.kind == KeyEventKind::Press {
+            if 
+                    key.modifiers == KeyModifiers::ALT &&
+                    key.code == KeyCode::Char('q') ||
+                    key.code == KeyCode::Esc
+            {
+                app.exit = true
+            }
 
-        if 
-                key.modifiers == KeyModifiers::ALT &&
-                key.code == KeyCode::Char('q') ||
-                key.code == KeyCode::Esc
-        {
-            app.exit = true
-        }
+            if 
+                key.code == KeyCode::Char('h') && key.modifiers == KeyModifiers::CONTROL ||
+                key.code == KeyCode::Backspace && key.modifiers == KeyModifiers::CONTROL
+            {
+                app.del_last_word()
+            } else {
+                match key.code {
+                    KeyCode::Char(char) => app.curr_text.push(char),
+                    KeyCode::Backspace => {
+                        app.curr_text.pop(); 
+                    },
 
-        if key.code == KeyCode::Char('h') && key.modifiers == KeyModifiers::CONTROL {
-            app.del_last_word()
-        } else {
-            match key.code {
-                KeyCode::Char(char) => app.curr_text.push(char),
-                KeyCode::Backspace => {
-                    app.curr_text.pop(); 
-                },
-
-                _ => ()
+                    _ => ()
+                }
             }
         }
     }
